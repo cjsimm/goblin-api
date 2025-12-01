37bfaba203f2ff046504c58ad7a15debb567d94a
cjsimm <14805972+cjsimm@users.noreply.github.com>
2025-12-01 16:07:14 +0000

    implement template write and push to origin for fleeting notes

--- src/api/zk.py
+++ src/api/zk.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, BackgroundTasks, Response
+from pydantic import BaseModel
+
+from src.obsidian import FLEETING_NOTE_TEMPLATE, sync_note_to_origin, write_template_to_obsidian_vault
+
+class TelegramUpdate(BaseModel):
+    """Schema representing an API Update, the main JSON format served from the
+    Telegram Bot API informing an application of updates to bot channels"""
+    ...
+
 
 zk = APIRouter(prefix="/zk")
 
 
 @zk.post("/fleeting")
-async def create() -> Response:
+async def create(background_tasks: BackgroundTasks) -> Response:
     """
     Create a fleeting zk note and add it to an obsidian vault in the unsorted category
     """
+    original_note = "placeholder"
+    note_filename = write_template_to_obsidian_vault(original_note, FLEETING_NOTE_TEMPLATE)
+    background_tasks.add_task(sync_note_to_origin, note_filename)
     return Response()
