a24339a6645c2ba16e43ae5742b66509a02a75ad
cjsimm <14805972+cjsimm@users.noreply.github.com>
2025-12-06 14:55:49 +0000

    add zk router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, FastAPI, Response
+from src.api.zk import zk
 
 app = FastAPI()
 
 v1_router = APIRouter(prefix="/api/v1")
+
+v1_router.include_router(zk)
 
 
 @v1_router.get("/status/checkhealth")
