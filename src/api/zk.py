329b56c9d622c3aa2da7b05ba625c2136fbdd829
cjsimm <14805972+cjsimm@users.noreply.github.com>
2025-12-27 17:23:34 +0000

    implement telegram bot zk endpoint. still undecided whether want to implement separate bot chats for different tasks, or have one master bot. a ZK bot would probably be the best encapsulation

--- src/api/zk.py
+++ src/api/zk.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, BackgroundTasks, Response
+from fastapi import APIRouter, BackgroundTasks, Body, HTTPException, Response
 from pydantic import BaseModel
 
 from src.constants import FLEETING_NOTE_PATH, MARKDOWN_COLLECTION_PATH
@@ -8,6 +8,7 @@ from src.markdown import (
     sync_note_to_origin,
     write_template_to_markdown_collection,
 )
+from src.services.telegram import queue_webhook_payload
 
 
 class TelegramUpdate(BaseModel):
@@ -30,3 +31,25 @@ async def create(background_tasks: BackgroundTasks) -> Response:
     new_file_relative_path = target_path.relative_to(MARKDOWN_COLLECTION_PATH)
     background_tasks.add_task(sync_note_to_origin, new_file_relative_path)
     return Response()
+
+
+# This endpoint needs to be protected with a signed key telegram offers a
+# header with a token that's set upon webhook setup
+# X-Telegram-Bot-Api-Secret-Token. on application startup, need to generate
+# this token, sign it with a key, and descrypt on every request through. On
+# localhost development, this url needs to also be exposed via ngrok or some
+# other local host tunneling app. Going to use the update polling for now to
+# push forward with development
+@zk.post("/telegram")
+async def bot(
+    payload: dict = Body(
+        ..., description="Update object defined in the Telegram Bot API", example=""
+    ),
+) -> Response:
+    """Webhook listeners for telegram fleeting note bot"""
+    try:
+        await queue_webhook_payload(payload)
+    except Exception:
+        # log here
+        raise HTTPException(400, "Could not parse update payload from Telegram")
+    return Response(status_code=201)
